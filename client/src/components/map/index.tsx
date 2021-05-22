be9f3efd9b68cd9b561a51a21955a627e6a63c1d
Ethan <hyeonseong04.lim@gmail.com>
2021-05-22 16:34:22 -0400

    poop backup at Sat May 22 2021 16:34:22 GMT-0400 (Eastern Daylight Time)

--- client/src/components/map/index.tsx
+++ client/src/components/map/index.tsx
@@ -6,10 +6,9 @@ import locationIcon from "@iconify/icons-mdi/map-marker"
 
 // https://blog.logrocket.com/a-practical-guide-to-integrating-google-maps-in-react/
 
-const LocationPin: React.FC<{[key: string]: unknown; text?: string}> = ({text}) => (
+const LocationPin: React.FC<{[key: string]: unknown}> = () => (
     <div className="pin">
         <Icon icon={locationIcon} className="pin-icon" />
-        <p className="pin-text">{text ?? ""}</p>
     </div>
 )
 
@@ -18,22 +17,19 @@ type MapProps = {
         lat: number
         lng: number
     }
-    address?: string
     zoom?: number
 }
 
 export class Map extends React.Component<MapProps> {
-    public defaultProps = {
+    public static defaultProps = {
         center: {lat: 55.75211384937568, lng: 37.617520856123846},
         zoom: 11,
-        address: "Moscow Kremlin, Dvortsovaya Street, Moscow, Russia, 103132",
     }
 
     public render = () => {
         const {
-            center = this.defaultProps.center,
-            zoom = this.defaultProps.zoom,
-            address = this.defaultProps.address,
+            center = Map.defaultProps.center,
+            zoom = Map.defaultProps.zoom,
         } = this.props
 
         return (
@@ -46,7 +42,7 @@ export class Map extends React.Component<MapProps> {
                         defaultCenter={center}
                         defaultZoom={zoom}
                     >
-                        <LocationPin lat={center.lat} lng={center.lng} text={address} />
+                        <LocationPin lat={center.lat} lng={center.lng} />
                     </GoogleMapReact>
                 </div>
             </div>
--- server/src/db/getData.js
+++ server/src/db/getData.js
@@ -1,6 +1,9 @@
 import {promises as fs} from "fs"
 
-// const result = await fs.readFile("./db.json", "utf-8")
-const result = JSON.parse(await fs.readFile("./db.json", "utf-8"))
+const getData = async () => {
+    const result = JSON.parse(await fs.readFile("./db.json", "utf-8"))
 
-console.log(result["Hello"])
+    return (result["Cities"]["Waterloo"])
+}
+
+getData()
