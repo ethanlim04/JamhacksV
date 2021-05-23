894133cced9e6bbce9f380ad05bd496e19b5cc79
Ethan <hyeonseong04.lim@gmail.com>
2021-05-22 21:28:20 -0400

    backup at Sat May 22 2021 21:28:20 GMT-0400 (Eastern Daylight Time)

--- backup-interval.bash
+++ backup-interval.bash
@@ -5,7 +5,7 @@ do
 	echo "Backing up..."
     ./backup.sh
 
-    echo -e "Next backup in 10 minutes at $(node -e 'console.log(new Date(Date.now() + 1_200_000).toString())') \n(currently $(node -e 'console.log(new Date().toString())'))"
+    echo -e "Next backup in 20 minutes at $(node -e 'console.log(new Date(Date.now() + 1_200_000).toString())') \n(currently $(node -e 'console.log(new Date().toString())'))"
 
     sleep 1200
     clear
