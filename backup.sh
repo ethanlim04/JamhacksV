90b4fd71942e2c020ece474d0159f2e6051098b9
Ethan <hyeonseong04.lim@gmail.com>
2021-05-22 21:24:16 -0400

    backup at Sat May 22 2021 21:24:16 GMT-0400 (Eastern Daylight Time)

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-msg="backup at $(node -e 'Math.random() > 0.9 ? console.log(Math.random() > 0.5 ? \"I am stupid\" : \"I pissed my pants\") : console.log((new Date()).toString())')"
+msg="backup at $(node -e 'Math.random() > 0.9 ? console.log(Math.random() > 0.5 ? '\''I am stupid'\'' : '\''I pissed my pants'\'') : console.log((new Date()).toString())')"
 
 git add .
 git commit -am"$msg"
